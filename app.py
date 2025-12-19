5e3fcce4cbb9fe542602abdc1dd2faf5430c4c32
Utkarsh9630 <tripathiutkarsh46@gmail.com>
2025-12-19 13:20:20 -0800

    Fix password hash column length

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class User(db.Model):
     id       = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email    = db.Column(db.String(120), unique=True, nullable=False)
-    pw_hash  = db.Column(db.String(128), nullable=False)
+    pw_hash  = db.Column(db.String(256), nullable=False)
     profiles = db.relationship("Profile", backref="user", lazy=True)
 
     def set_password(self, pw):
